f7856600405b82941501f778e5ad50a2789b68c3
bongani10 <bonganigadlela@gmail.com>
2024-05-10 12:45:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SalaryAppUJ
+{
+    internal class Program
+    {
+        //program execution begins here
+        static void Main(string[] args)
+        {
+            double revenue;
+            double cost;
+            double profit;
+
+            Console.WriteLine("Enter revenue");
+            revenue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter cost");
+            cost = Convert.ToDouble(Console.ReadLine());
+
+            profit = revenue - cost;
+
+            Console.WriteLine("Profit is \tR" + profit.ToString("F2"));
+
+        }//end main
+    }//end class
+}//end namespace
